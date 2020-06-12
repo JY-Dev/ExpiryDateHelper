9d6f367ed65a233135351734db711d388c4f98fc
jaeyoung <kjy0302014@gmail.com>
2020-06-12 17:10:03 +0900

    - 알람기능 제외하고 모든기능 구현완료

--- app/src/main/java/com/jydev/expirydatehelper/util/DataProcess.kt
+++ app/src/main/java/com/jydev/expirydatehelper/util/DataProcess.kt
@@ -13,7 +13,6 @@ import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
 import com.jydev.expirydatehelper.R
 import com.jydev.expirydatehelper.data.FoodData
-import com.jydev.expirydatehelper.noti.AlarmBR
 import com.jydev.expirydatehelper.room.foodData.FoodDataDB
 import com.jydev.expirydatehelper.room.foodData.FoodDataRoom
 import io.reactivex.Observable
@@ -94,57 +93,4 @@ class DataProcess {
         return gson.fromJson(jsonArray, type.type)
     }
 
-    fun startBroadcast(context: Context, millis: Long) {
-        //브로드캐스트 intent 설정
-        val intent = Intent(context, AlarmBR::class.java)
-        intent.putExtra("test", millis)
-        //pendingintent 설정
-        val pendingIntent =
-            PendingIntent.getBroadcast(
-                context.applicationContext,
-                detailedTime(millis).toInt(),
-                intent,
-                PendingIntent.FLAG_UPDATE_CURRENT
-            )
-        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
-        //알람 매니져 set 특정시간에 발동되도록
-        when {
-            Build.VERSION.SDK_INT >= 23 -> alarmManager.setExactAndAllowWhileIdle(
-                AlarmManager.RTC_WAKEUP,
-                detailedTime(millis),
-                pendingIntent
-            )
-            Build.VERSION.SDK_INT >= 19 -> alarmManager.setExact(
-                AlarmManager.RTC_WAKEUP,
-                detailedTime(millis),
-                pendingIntent
-            )
-            else -> alarmManager.setRepeating(
-                AlarmManager.RTC_WAKEUP, detailedTime(millis),
-                AlarmManager.INTERVAL_DAY, pendingIntent
-            )
-        }
-
-    }
-
-    fun showNotification(context: Context, title: String, task: String, id: Int) {
-
-        val notificationManager =
-            context.applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(
-                notiChannelId,
-                notiChannelName,
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-            notificationManager.createNotificationChannel(notificationChannel)
-        }
-        val notification =
-            NotificationCompat.Builder(context.applicationContext, notiChannelId)
-                .setContentTitle(title)
-                .setContentText(task)
-                .setSmallIcon(R.mipmap.ic_launcher)
-        notificationManager.notify(id, notification.build())
-    }
 }
